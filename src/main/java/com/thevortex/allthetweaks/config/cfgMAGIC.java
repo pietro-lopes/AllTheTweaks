041f4f7d46377cc87cc7682316a70373a45ae3ea
thevortex <james.g.bowley@gmail.com>
2025-01-20 18:40:20 -0700

    minor tweak to configs

--- src/main/java/com/thevortex/allthetweaks/config/cfgMAGIC.java
+++ src/main/java/com/thevortex/allthetweaks/config/cfgMAGIC.java
@@ -5,8 +5,8 @@ import net.minecraft.resources.ResourceLocation;
 
 public class cfgMAGIC {
 
-	public static long IPCC = 799149850566459422L;
-	public static String ATM = "atm-m2";
+	public static long IPCC = 1327451857668411452L;
+	public static String ATM = "atm-m3";
 	public static String DISPLAY = "All The Mods: Arcana";
 	public static ResourceLocation BACKGROUND = ResourceLocation.fromNamespaceAndPath(AllTheTweaks.MODID,"textures/gui/title/background.3.png");
 }
